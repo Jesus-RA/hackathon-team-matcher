dfd08ff1f0a7bf5fe67a28d1c758045015395f15
Jesus Ramirez <jesus.ra98@hotmail.com>
2025-05-11 16:46:10 -0600

    Make supabase.js lib file read supabase env variables from astro:env/client

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,3 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from 'astro:env/client'
 
-export const supabase = createClient(import.meta.env.SUPABASE_URL, import.meta.env.SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
