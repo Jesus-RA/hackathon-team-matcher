03c6633d00002698d8ca8a89fca66129c5806b04
Jesus Ramirez <jesus.ra98@hotmail.com>
2025-05-20 13:09:35 -0600

    Add workaround to Connection Requests page to display recipient info if current user is the requester, and display requester info if current user is the recipient

--- src/components/connection-requests/ConnectionRequestsList.vue
+++ src/components/connection-requests/ConnectionRequestsList.vue
@@ -4,7 +4,8 @@
         :key="request.id"
         class="light-dark-theme rounded-lg p-6 shadow-lg outline outline-gray-100 dark:outline-none"
     >
-        <div class="flex flex-col md:flex-row justify-between gap-4">
+        <!-- If current user is requester, show recipient info -->
+        <div v-show="request.requester_user_id === authStore.userId" class="flex flex-col md:flex-row justify-between gap-4">
             <div class="flex flex-col gap-3">
                 <div>
                     <div class="flex items-center justify-between gap-x-2">
@@ -21,6 +22,25 @@
 
             <ConnectionRequestActions :request="request" @request-cancelled="handleRequestCancelled" />
         </div>
+
+        <!-- If current user is recipient, show requester info -->
+        <div v-show="request.recipient_user_id === authStore.userId" class="flex flex-col md:flex-row justify-between gap-4">
+            <div class="flex flex-col gap-3">
+                <div>
+                    <div class="flex items-center justify-between gap-x-2">
+                        <h3 class="text-lg font-medium">{{ request.requester.name }}</h3>
+                        <StatusBadge :status="request.status" class="flex md:hidden" />
+                    </div>
+                    <p class="text-sm text-gray-600 dark:text-gray-400">{{ request.requester.job_title }}</p>
+                    <p v-if="request.status === 'accepted' && request.requester.email" class="flex items-center gap-x-2 text-sm text-gray-600 dark:text-gray-400">
+                        <Mail class="size-4" /> {{ request.requester.email }}
+                    </p>
+                </div>
+                <p class="text-gray-600 dark:text-gray-400">{{ request.requester.bio }}</p>
+            </div>
+
+            <ConnectionRequestActions :request="request" @request-cancelled="handleRequestCancelled" />
+        </div>
     </article>
 </template>
 
@@ -31,6 +51,10 @@ import Mail from '@/components/icons/Mail.vue';
 
 
 import { ref } from 'vue';
+import { $authStore } from '@clerk/astro/client';
+import { useStore } from '@nanostores/vue';
+
+const authStore = useStore($authStore);
 
 const { requests } = defineProps({
     requests: {
